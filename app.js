025b20485caeff0b59f54b8d40ee749fafb7391a
Adam <adam.moftah@gmail.com>
2020-04-21 12:05:42 -0400

    switch key uri

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
   });
-  console.log("this is running!!!!!!!!!!!! ");
 }
 
 mongoose
