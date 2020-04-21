ff2e806017c996b3579690af292d0b972ab8bc9f
Adam <adam.moftah@gmail.com>
2020-04-21 10:27:57 -0400

    prepare for deployment to heroku

--- app.js
+++ app.js
@@ -2,23 +2,31 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
 // const bodyParser = require('body-parser');
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/public"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+  });
+}
+
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
-io.on('connection', (socket) => {
-    console.log("Connected to Socket yay!" + socket.id);
-    socket.emit('init', {});
+io.on("connection", (socket) => {
+  console.log("Connected to Socket yay!" + socket.id);
+  socket.emit("init", {});
 
-    socket.on('multiplyNum', (num) => {
-        console.log(`Received message from client: ${num}. Returning new message`);
-        socket.emit('message', num * 2);
-    })
+  socket.on("multiplyNum", (num) => {
+    console.log(`Received message from client: ${num}. Returning new message`);
+    socket.emit("message", num * 2);
+  });
 });
 
 // app.use(bodyParser.json());
