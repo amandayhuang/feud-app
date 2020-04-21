44067a80a3801357c471d3dc168489febebfde50
Jared Meier <meier.jared@gmail.com>
2020-04-21 14:22:07 -0400

    Add basic room socket setup

--- app.js
+++ app.js
@@ -4,25 +4,34 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-// const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-io.on('connection', (socket) => {
-    console.log("Connected to Socket yay!" + socket.id);
-    socket.emit('init', {});
 
-    socket.on('multiplyNum', (num) => {
-        console.log(`Received message from client: ${num}. Returning new message`);
-        socket.emit('message', num * 2);
-    })
+let rooms = [];
+io.on('connect', (socket) => {
+    console.log("Connected to Socket yay! Socket id: " + socket.id);
+
+    socket.on('join', (room) => {
+        socket.join(room);
+        io.to(room).emit('receiveConsoleMessage', `You joined room ${room}`);
+        rooms.push(room);
+    });
 });
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended:true }));
+// set up dummy gameState object
+const gameState = { players: ["player 1", "player 2"], score: 100 };
+
+// regularly update all rooms with the gameState
+setInterval(() => {
+    rooms.forEach(room => {
+        io.to(room).emit('receiveConsoleMessage', `Here is your update for room ${room}`);
+        io.to(room).emit('receiveGameState', gameState);
+    })
+}, 2000);
 
 const port = process.env.PORT || 5000;
 http.listen(port, () => console.log(`Listening on port ${port}`));
