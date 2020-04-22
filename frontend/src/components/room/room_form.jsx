1c9f98b64932f22df40bcc2fb3074e39b55dadb6
Adam <adam.moftah@gmail.com>
2020-04-22 14:35:32 -0400

    change room form logic

--- frontend/src/components/room/room_form.jsx
+++ frontend/src/components/room/room_form.jsx
@@ -1,65 +1,107 @@
-import React from 'react';
+import React from "react";
 
 class RoomForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            action: 'join',
-            roomId: '',
-            nickname: ''
-        }
-        this.handleRoomJoin = this.handleRoomJoin.bind(this);
-        this.updateForm = this.updateForm.bind(this);
-        this.switchForm = this.switchForm.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      action: "join",
+      roomId: "",
+      nickname: "",
+      phase: "decision",
+    };
+    this.handleRoomJoin = this.handleRoomJoin.bind(this);
+    this.updateForm = this.updateForm.bind(this);
+    this.switchForm = this.switchForm.bind(this);
+  }
+
+  updateForm(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  handleRoomJoin(e) {
+    e.preventDefault();
+    const { action, roomId, nickname } = this.state;
+    this.props.handleRoomJoin(action, roomId, nickname);
+    this.setState({ roomId: "", nickname: "" });
+  }
+
+  switchForm() {
+    const newForm = this.state.action === "join" ? "create" : "join";
+    this.setState({ action: newForm });
+  }
+
+  render() {
+    const { action, phase } = this.state;
+    let buttonText,
+      otherButtonText = "";
+    let chooseForm, enterRoomCode, enterNickname, roomForm;
+
+    if (action === "join") {
+      buttonText = "Join Room";
+      otherButtonText = "Create Room";
+    } else {
+      buttonText = "Create Room";
+      otherButtonText = "Join Room";
     }
 
-    updateForm(field) {
-        return (e) => {
-            this.setState({ [field]: e.target.value })
-        }
+    if (phase === "decision") {
+      chooseForm = (
+        <div>
+          <button
+            onClick={() => {
+              this.setState({ phase: "decided", action: "join" });
+            }}
+          >
+            JOIN ROOM
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ phase: "decided", action: "create" });
+            }}
+          >
+            CREATE ROOM
+          </button>
+        </div>
+      );
+    } else if (phase === "decided") {
+      enterRoomCode = (
+        <input
+          type="text"
+          placeholder="Enter room code"
+          value={this.state.roomId}
+          onChange={this.updateForm("roomId")}
+        ></input>
+      );
+      enterNickname = (
+        <input
+          type="text"
+          placeholder="Nickname"
+          value={this.state.nickname}
+          onChange={this.updateForm("nickname")}
+        ></input>
+      );
+
+      roomForm = (
+        <>
+          <form onSubmit={this.handleRoomJoin} className="room-form">
+            {enterRoomCode}
+            {enterNickname}
+            <button type="submit">{buttonText}</button>
+          </form>
+          <button onClick={this.switchForm}>Or {otherButtonText}...</button>
+        </>
+      );
     }
 
-    handleRoomJoin(e) {
-        e.preventDefault();
-        const { action, roomId, nickname } = this.state;
-        this.props.handleRoomJoin(action, roomId, nickname);
-        this.setState({ roomId: '', nickname: ''});
-    }
-
-    switchForm() {
-        const newForm = this.state.action === 'join' ? 'create' : 'join';
-        this.setState({ action: newForm});
-    }
-
-    render() {
-        const { action } = this.state;
-        let buttonText, otherButtonText = '';
-        if (action === 'join') {
-            buttonText = "Join Room";
-            otherButtonText = "Create Room"
-        }  else {
-            buttonText = "Create Room";
-            otherButtonText = "Join Room"
-        }
-        return (
-            <>
-                <form onSubmit={this.handleRoomJoin} className="room-form">
-                    <input type="text"
-                        placeholder="Enter room code"
-                        value={this.state.roomId}
-                        onChange={this.updateForm('roomId')}>
-                    </input>
-                    <input type="text"
-                        placeholder="Nickname"
-                        value={this.state.nickname}
-                        onChange={this.updateForm('nickname')}>
-                    </input>
-                    <button type="submit">{buttonText}</button>
-                </form>
-                <button onClick={this.switchForm}>Or {otherButtonText}...</button>
-            </>
-        )
-    }
+    return (
+      <>
+        {chooseForm}
+        {roomForm}
+      </>
+    );
+  }
 }
 
 export default RoomForm;
