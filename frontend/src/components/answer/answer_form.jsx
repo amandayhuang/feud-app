7546950a53d264dd30421b6d70dbade4b6e176c5
Adam <adam.moftah@gmail.com>
2020-04-24 13:53:24 -0400

    fix issue with opacity showing on heroku

--- frontend/src/components/answer/answer_form.jsx
+++ frontend/src/components/answer/answer_form.jsx
@@ -1,81 +1,82 @@
-import React from 'react';
+import React from "react";
 
 class AnswerForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            answer: '',
-            timer: ''
-        }
-        this.handleAnswerSubmit = this.handleAnswerSubmit.bind(this);
-        this.updateForm = this.updateForm.bind(this);
-        this.answerTimer = null;
-        this.answerTimerLength = 100;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: "",
+      timer: "",
+    };
+    this.handleAnswerSubmit = this.handleAnswerSubmit.bind(this);
+    this.updateForm = this.updateForm.bind(this);
+    this.answerTimer = null;
+    this.answerTimerLength = 15;
+  }
 
-    componentDidMount() {
-        this.startTimer();
-    }
+  componentDidMount() {
+    this.startTimer();
+  }
 
-    startTimer() {
-        clearInterval(this.answerTimer);
-        this.setState({ timer: this.answerTimerLength })
-        this.answerTimer = setInterval(() => this.countdown(), 1000);
-    }
+  startTimer() {
+    clearInterval(this.answerTimer);
+    this.setState({ timer: this.answerTimerLength });
+    this.answerTimer = setInterval(() => this.countdown(), 1000);
+  }
 
-    countdown() {
-        let { timer } = this.state;
-        timer --;
-        if (timer === 0) {
-            this.timerDone();
-        } else {
-            this.setState({ timer: timer});
-        }
+  countdown() {
+    let { timer } = this.state;
+    timer--;
+    if (timer === 0) {
+      this.timerDone();
+    } else {
+      this.setState({ timer: timer });
     }
+  }
 
-    timerDone() {
-        clearInterval(this.answerTimer);
-        this.setState({ timer: 0 });
-        this.handleAutoSubmit();
-    }
+  timerDone() {
+    clearInterval(this.answerTimer);
+    this.setState({ timer: 0 });
+    this.handleAutoSubmit();
+  }
 
-    updateForm(field) {
-        return (e) => {
-            this.setState({ [field]: e.target.value })
-        }
-    }
+  updateForm(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
 
-    handleAutoSubmit() {
-        const { answer } = this.state;
-        this.props.handleAnswerSubmit(answer);
-        this.setState({ answer: '' });
-        this.startTimer();
-    }
+  handleAutoSubmit() {
+    const { answer } = this.state;
+    this.props.handleAnswerSubmit(answer);
+    this.setState({ answer: "" });
+    this.startTimer();
+  }
 
-    handleAnswerSubmit(e) {
-        e.preventDefault();
-        const { answer } = this.state;
-        this.props.handleAnswerSubmit(answer);
-        this.setState({ answer: '' });
-        this.startTimer();
-    }
+  handleAnswerSubmit(e) {
+    e.preventDefault();
+    const { answer } = this.state;
+    this.props.handleAnswerSubmit(answer);
+    this.setState({ answer: "" });
+    this.startTimer();
+  }
 
-    render() {
-        const { timer } = this.state;
-        return (
-            <>
-                <h2 className="timer">{timer}</h2>
-                <form onSubmit={this.handleAnswerSubmit} className="answer-form">
-                    <input type="text"
-                        placeholder="Enter answer"
-                        value={this.state.answer}
-                        onChange={this.updateForm('answer')}>
-                    </input>
-                    <button type="submit">SUBMIT</button>
-                </form>
-            </>
-        )
-    }
+  render() {
+    const { timer } = this.state;
+    return (
+      <>
+        <h2 className="timer">{timer}</h2>
+        <form onSubmit={this.handleAnswerSubmit} className="answer-form">
+          <input
+            type="text"
+            placeholder="Enter answer"
+            value={this.state.answer}
+            onChange={this.updateForm("answer")}
+          ></input>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default AnswerForm;
